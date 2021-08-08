b8bd53b4133f5095045ac74e8c665236dc00c1ee
Olga Tiutiunnyk <izachok@gmail.com>
2021-08-08 19:24:48 +0300

    add 2- Statistics

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
 // import logo from './logo.svg';
 import './App.css';
 import Profile from './components/Profile/Profile';
+import Statistics from './components/Statistics/Statistics';
+
 import user from './data/user.json';
+import statisticalData from './data/statistical-data.json';
 
 function App() {
   return (
@@ -13,6 +16,12 @@ function App() {
         avatar={user.avatar}
         stats={user.stats}
       />
+      <div>
+        <Statistics title="Upload stats" stats={statisticalData} />
+      </div>
+      <div>
+        <Statistics stats={statisticalData} />
+      </div>
     </div>
   );
 }
---
+++ src/components/Statistics/Statistics.js
@@ -0,0 +1,43 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import s from './Statistics.module.css';
+
+function Statistics({ title, stats }) {
+  return (
+    <section className={s.container}>
+      {title && <h2 className={s.title}>{title}</h2>}
+      <ul className={s.list}>
+        {stats.map(stat => (
+          <li
+            className={s.listItem}
+            key={stat.id}
+            style={{ backgroundColor: getRandomRGBColor() }}
+          >
+            <span className={s.label}>{stat.label}</span>
+            <span className={s.percentage}>{stat.percentage}%</span>
+          </li>
+        ))}
+      </ul>
+    </section>
+  );
+}
+
+Statistics.propTypes = {
+  title: PropTypes.string,
+  stats: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.string.isRequired,
+      label: PropTypes.string.isRequired,
+      percentage: PropTypes.number.isRequired,
+    }).isRequired,
+  ).isRequired,
+};
+
+function getRandomRGBColor() {
+  //to prevent color be too light
+  return `rgb(${Math.floor(Math.random() * 235)}, ${Math.floor(
+    Math.random() * 235,
+  )}, ${Math.floor(Math.random() * 235)})`;
+}
+
+export default Statistics;
